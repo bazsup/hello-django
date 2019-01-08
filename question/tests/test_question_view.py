732fe79e76a3b7367c88d8d0c344c8d06effad45
bazsup <supawit.ruen@mail.kmutt.ac.th>
2019-01-08 17:12:43 +0700

    :white_check_mark: add tests admin list_display, model, views

---
+++ question/tests/test_question_view.py
@@ -0,0 +1,25 @@
+import json
+
+from django.test import TestCase
+from django.urls import reverse
+
+from ..models import Choice, Question
+
+class QuestionGetViewTests(TestCase):
+    def test_question_response_return_correctly(self):
+        question = Question.objects.create(question='1 + 1 = ?')
+        choice = Choice.objects.create(question=question, title='หนึ่ง')
+        expected = [
+            {
+                'question_pk': 1,
+                'question': '1 + 1 = ?',
+                'choices': [choice]
+            }
+        ]
+
+        response = self.client.get(reverse('question'))        
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, "1 + 1 = ?")
+
+        question_context = response.context['question_list']
+        self.assertEqual(question_context, expected)
