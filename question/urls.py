2b4ae9de66f0101a479d8f794ec733abd05b55bf
bazsup <supawit.ruen@mail.kmutt.ac.th>
2019-01-08 17:11:51 +0700

    :sparkles: add reverse_name (url name)

--- question/urls.py
+++ question/urls.py
@@ -5,7 +5,7 @@ from question import views as questionView
 from question.views import QuestionView
 
 urlpatterns = [
-    path('question/', QuestionView.as_view()),
+    path('question/', QuestionView.as_view(), name="question"),
     path('answer/', questionView.answer),
     path('answer/reset/', questionView.reset_answer),
 ]
